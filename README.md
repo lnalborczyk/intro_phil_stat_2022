d233eb3e0f6e09f14d4a1ee0e41599a50e32abc5
lnalborczyk <ladislas.nalborczyk@gmail.com>
2022-11-01 11:57:53 +0100

    minor updates to slides

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Moving to a World beyond p < .05: An introduction to the philosophy of statistics
 
-Slides for the course given for the first MASCO in Aix-Marseille Univ (2022).
+Course given for Master students in Cognitive Science at Aix-Marseille Univ (November 2022).
+
+<img src="figures/phacking.png" align="center" width="500px">
---
+++ pdf_from_slides.R
@@ -0,0 +1,10 @@
+########################################################
+# Generating pdf from quarto (revealjs) slides
+# ---------------------------------------------------
+# Written by Ladislas Nalborczyk
+# E-mail: ladislas.nalborczyk@gmail.com
+# Last updated on November 1, 2022
+###############################################
+
+library(pagedown)
+chrome_print(input = "slides.html", format = "pdf", timeout = 60)
